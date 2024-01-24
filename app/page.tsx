73e6ef69bfe07de3fa1adcca83e484b0d59db3b6
nayhtet20 <nayhtetzaw@geniefintech.com>
2024-01-25 04:20:46 +0630

    fix: update style

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <SessionProvider>
       <main className="flex min-h-screen flex-col items-center p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm flex">
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             NextJs + Next-Auth + Vercel/Postgres
           </p>
         </div>
